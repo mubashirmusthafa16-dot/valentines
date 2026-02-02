d10fb1df50c03693c3fd817b381f256c9f2bfbc0
mubashirmusthafa16-dot <mubashirmusthafa16@gmail.com>
2026-02-02 22:18:45 +0400

    Update script.js

--- script.js
+++ script.js
@@ -11,7 +11,7 @@ function no() {
   if (noCount === 1) {
     buttons.innerHTML = `
       <button onclick="yes()">Yes💖</button>
-      <button id="noBtn" onmouseover="runAway()">Really??</button>
+      <button id="noBtn" onmouseover="runAway()">Really??🥺</button>
     `;
   } if (noCount === 2) {
     buttons.innerHTML = `
